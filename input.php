2853059279096ae8e04052c38ece419d11711b86
DMITRY1IE <lozhkin315@gmail.com>
2023-07-19 18:56:30 +0300

    conec

--- input.php
+++ input.php
@@ -1,47 +1,28 @@
 <?php
+$file = $_FILES["file"];
+$file_send = $file["name"];
 if(isset($_POST["fio"]) && isset($_POST["email"]) && isset($_POST["phone"]) 
-    && isset($_POST["info"]) && isset($_FILES["file"])) 
-{
-    $fio = htmlentities($_POST["fio"]);
-    $email = htmlentities($_POST["email"]);
-    $phone = htmlentities($_POST["phone"]);
-    $info = htmlentities($_POST["info"]);
-    $file = $_FILES["file"];
-    $file = $file['name'];
-    $output ="
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>Обработанные данные</title>
-    <link rel='stylesheet' href='css/style.css'>
-
-    </head>
-        <body>
-        <div class='form-container'>
-            <div class='form-output'>
-                <div style='font-size:30px;' class='form-inner'>
-                    Вас зовут:<p> $fio</p>
-                </div>
-                <div style='font-size:30px;' class='form-inner'>
-                    Ваш email:<p> $email</p>
-                </div>
-                <div style='font-size:30px;' class='form-inner'>
-                    Ваш телефон:<p> $phone</p> 
-                </div>
-                <div  style='font-size:30px;' class='form-inner'>
-                    Информация об ошибке:<p> $info</p> 
-                </div>
-                <div style='font-size:30px;' class='form-inner'>
-                    Скриншот ошибки:<p> $file</p>
-                </div>
-            </div>
-        </div>
-        </body>
-    </html>";
-    echo $output;
-}
-else
+    && isset($_POST["info"]) && isset($_FILES["file"]) && $file['size'] < 1*1024*1024) {
+    
+    $message = 'ФИО: ' . $_POST["fio"] . ' ';
+    $message .= 'Email: ' . $_POST['email'] . ' ';
+    $message .= 'Телефон: ' . $_POST['phone'] . ' ';
+    $message .= 'Информация об ошибке: ' . $_POST['info'] . ' ';
+    $message .= 'Скриншот ошибки: ' . $file_send . ' ';
+    
+    $mailTo = "dmitry1ie@mail.ru"; // Ваш e-mail
+    $subject = "Информация об ошибке"; 
+    $headers= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8\r\n";
+    $headers .= "From: info@test.site <info@test.site>\r\n";//почта с хоста
+    if(mail($mailTo, $subject, $message, $headers)) {
+        echo "Спасибо, ".$_POST['fio'].", мы свяжемся с вами в самое ближайшее время!";
+        print_r($_FILES["file"]);
+    } else {
+        echo "Сообщение не отправлено!"; 
+    }
+}else
 {   
-    echo "Введенные данные некорректны";
+   echo "Введенные данные некорректны";
 }
 ?>
